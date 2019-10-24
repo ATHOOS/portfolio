c087c2d08bfaf044130814b95fcc366cdaf34372
ATHOOS <adrien.chelle0712@hotmail.fr>
2019-10-24 22:47:08 +0200

    update contact

--- html/template/mailer/form_setting.php
+++ html/template/mailer/form_setting.php
@@ -1,8 +1,8 @@
 <?php
 	/*Form settings*/
-	$subj = "New message from the site 'Cvio - Resume/CV Template'"; //letter subject
-	$to = 'beshleyua@gmail.com'; // Enter Your E-mail
-	$from = 'admin@you-site-name.com'; // Admin e-mail
-	$fromName = 'Your Company Name'; // Your company name
+	$subj = "Message du Portfolio"; //letter subject
+	$to = 'adrien.chelle0712@hotmail.fr'; // Enter Your E-mail
+	$from = 'contact@portfolio.com'; // Admin e-mail
+	$fromName = 'Portfolio'; // Your company name
 	$charset = 'UTF-8';
 ?>
